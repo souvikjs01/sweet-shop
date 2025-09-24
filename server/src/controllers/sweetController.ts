61388703a4fe4f393cb731ad7e1f5be3049a44c1
souvikjs01 <souvik741156@gmail.com>
2025-09-25 00:05:59 +0530

    sweet controller and routes

--- server/src/controllers/sweetController.ts
+++ server/src/controllers/sweetController.ts
@@ -110,3 +110,225 @@ export const updateSweet = async (req: Request, res: Response) => {
         });
     }
 };
+
+
+export const searchSweets = async (req: Request, res: Response) => {
+    try {
+        const { name, category, minPrice, maxPrice } = req.query;
+
+        const sweets = await prisma.sweet.findMany({
+            where: {
+                AND: [
+                name
+                    ?   {
+                            name: {
+                                contains: String(name),
+                                mode: "insensitive",
+                            },
+                        }
+                    :   {},
+                category
+                    ?   {
+                            category: {
+                                contains: String(category),
+                                mode: "insensitive",
+                            },
+                        }
+                    : {},
+                minPrice
+                    ?   {
+                            price: {
+                                gte: Number(minPrice),
+                            },
+                        }
+                    : {},
+                maxPrice
+                    ?   {
+                            price: {
+                                lte: Number(maxPrice),
+                            },
+                        }
+                    : {},
+                ],
+            },
+        });
+
+        res.status(200).json({ 
+            success: true,
+            data: sweets 
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ 
+            success: false,          
+            message: "Error searching sweets" 
+        });
+    }
+};
+
+
+export const deleteSweet = async (req: Request, res: Response) => {
+  try {
+        const role = req.user?.role
+        if (role !== "ADMIN") {
+            res.status(401).json({
+                success: false,
+                error: "unauthorized"
+            })
+            return
+        }
+
+        const sweetId = req.params.id;
+
+        // Check if sweet exists
+        const existingSweet = await prisma.sweet.findUnique({
+            where: { 
+                id: sweetId 
+            },
+        });
+
+        if (!existingSweet) {
+            res.status(404).json({
+                success: false, 
+                error: "Sweet not found" 
+            });
+            return
+        }
+
+        await prisma.sweet.delete({
+            where: { 
+                id: sweetId 
+            },
+        });
+
+        res.status(200).json({
+            success: true, 
+            message: "Sweet deleted successfully" 
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ 
+            success: false,
+            error: "Internal server error" 
+        });
+    }
+};
+
+
+
+export const purchaseSweet = async (req: Request, res: Response) => {
+  try {
+        const sweetId = req.params.id;
+        const qnty = req.body.quantity;
+
+        // Find sweet
+        const sweet = await prisma.sweet.findUnique({
+            where: { 
+                id: sweetId 
+            },
+        });
+
+        if (!sweet) {
+            res.status(404).json({ 
+                success: false,
+                error: "Sweet not found" 
+            });
+            return
+        }
+
+        if (sweet.quantity <= 0 || sweet.quantity < qnty) {
+            res.status(400).json({ 
+                success: false,
+                error: "Sweet is out of stock" 
+            });
+            return
+        }
+
+        // Decrease quantity by 1
+        const updatedSweet = await prisma.sweet.update({
+            where: { 
+                id: sweetId 
+            },
+            data: {
+                quantity: { 
+                    decrement: qnty
+                },
+            },
+        });
+
+        res.status(200).json({
+            success: true,    
+            amount: Number(sweet.price) * qnty,
+            message: "Sweet purchased successfully",
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ 
+            success: false,
+            error: "Internal server error" 
+        });
+    }
+};
+
+
+export const restockSweet = async (req: Request, res: Response) => {
+    try {
+        const userRole = req.user?.role
+        if (userRole !== "ADMIN") {
+            res.status(401).json({
+                success: false,
+                error: "unauthorized"
+            })
+            return
+        }
+
+        const sweetId = req.params.id;
+        const qnty = req.body.quantity; 
+
+        if (!qnty || qnty <= 0) {
+            res.status(400).json({ 
+                success: false,
+                error: "Restock amount must be greater than 0" 
+            });
+            return
+        }
+
+        // Check if sweet exists
+        const sweet = await prisma.sweet.findUnique({
+            where: { 
+                id: sweetId 
+            },
+        });
+
+        if (!sweet) {
+            res.status(404).json({ 
+                success: false,
+                error: "Sweet not found" 
+            });
+            return
+        }
+
+        // Increase quantity
+        const updatedSweet = await prisma.sweet.update({
+            where: { 
+                id: sweetId 
+            },
+            data: {
+                quantity: { 
+                    increment: qnty 
+                },
+            },
+        });
+
+        res.status(200).json({
+            success: true,
+            sweet: updatedSweet,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ 
+            message: "Internal server error" 
+        });
+    }
+};
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,7 +1,15 @@
 import express from "express"
 import { login, register } from "../controllers/authController";
 import { authenticate } from "../middlewares/middleware";
-import { addSweet, listSweets, updateSweet } from "../controllers/sweetController";
+import { 
+    addSweet, 
+    deleteSweet, 
+    listSweets, 
+    purchaseSweet, 
+    restockSweet, 
+    searchSweets, 
+    updateSweet 
+} from "../controllers/sweetController";
 
 const router = express.Router()
 
@@ -11,5 +19,10 @@ router.post("/auth/login", login);
 router.post("/sweets", authenticate, addSweet)
 router.get("/sweets", authenticate, listSweets)
 router.put("/sweets/:id", authenticate, updateSweet)
+router.get("/sweets/search", authenticate, searchSweets)
+router.delete("/sweets/:id", authenticate, deleteSweet)
+
+router.post("/sweets/:id/purchase", authenticate, purchaseSweet)
+router.post("/sweets/:id/restock", authenticate, restockSweet)
 
 export default router
